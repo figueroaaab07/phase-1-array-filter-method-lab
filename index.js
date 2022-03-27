a266f607073a15598139fb966c019d7e1653eb77
Alejandro Luis Figueroa Cevallos <figueroaaab07@gmail.com>
2022-03-26 21:44:01 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    const nameMatch = drivers.filter(function (element) {
+        return element.toUpperCase() === name.toUpperCase();
+    })
+    return nameMatch;
+}
+
+function fuzzyMatch(drivers, letters) {
+    const lettersMatch = drivers.filter(function (element) {
+        return element.toUpperCase().startsWith(letters.toUpperCase());
+    })
+    return lettersMatch;
+}
+
+function matchName(drivers, name) {
+    const nameMatch = drivers.filter(function (element) {
+        return element.name === name;
+    })
+    return nameMatch;
+}
